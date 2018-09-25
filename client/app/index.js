2fab0b2bf0e75aec3f001b659bbde84de1466ead
AminuSufi585 <aminukano585@gmail.com>
2018-09-25 20:48:52 +0100

    Initial commit

---
+++ client/app/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Basic Setup of Tech Events Client Side</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
